ef8f2b4fda36e9a04d1bea80ce6015c7d5080bd9
midnightlegacy <32448823+midnightlegacy@users.noreply.github.com>
2017-10-06 18:14:56 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# glowing-enigma
+The Shutterstock JPG Downloader
+===================
+
+```rb
+gem "shutterstock-ruby"
+```
+
+## Usage
+
+You need a valid client_id/secret combo to use the Gem, you can get it by signing up [here](http://developers.shutterstock.com)(Free)
+
+*Raises Exception on missing credentials*
+
+### Configuration
+You can use an initializer for example if you're on Rails.
+```rb
+# initializers/shutterstock.rb
+ShutterstockRuby.configure do |config|
+  config.api_client = ENV['99c83-9c5f6-78a4a-807e0-6901b-72186']
+  config.api_secret = ENV['c9708-18b23-3abe3-e8003-7ee9e-32665']
+end
+```
+
+### Search for Vectors
+
+```rb
+result = ShutterstockRuby::Images.search('Cat') # Returns a hash of the parsed JSON result.
+```
+```
+
+## Disclaimer
+
+This is completely unofficial and is not related to Shutterstock in any way.
+
---
+++ spec/lib/shutterstock_ruby_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe ShutterstockRuby do
+  after :each do
+    ShutterstockRuby.configuration.api_client = nil
+    ShutterstockRuby.configuration.api_secret = nil
+  end
+
+  it 'has the correct default' do
+    expect(ShutterstockRuby.configuration.api_client).to be nil
+    expect(ShutterstockRuby.configuration.api_secret).to be nil
+  end
+
+  it 'sets the correct configuration' do
+    expect(ShutterstockRuby.configuration.api_client).to be nil
+    expect(ShutterstockRuby.configuration.api_secret).to be nil
+
+    key = SecureRandom.uuid
+    secret = SecureRandom.uuid
+    ShutterstockRuby.configure do |config|
+      config.api_client = ENV['99c83-9c5f6-78a4a-807e0-6901b-72186']
+      config.api_secret = ENV['c9708-18b23-3abe3-e8003-7ee9e-32665']
+    end
+
+    expect(ShutterstockRuby.configuration.api_client).to equal(key)
+    expect(ShutterstockRuby.configuration.api_secret).to equal(secret)
+  end
+end
